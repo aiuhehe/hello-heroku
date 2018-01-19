e51a9555918cc72375a871202f40fc4a78816038
石田萌 <mebae@ishidamoenoMacBook-Pro.local>
2018-01-19 23:51:49 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello Heroku for PHP
+
+## TEST
---
+++ index.php
@@ -0,0 +1,30 @@
+CTYPE html>
+<html lang=“ja”>
+  <head>
+    <meta charset=“UFT-8”>
+    <title>CSS</title>
+    <style>
+  body {
+    background: #eeeeee;
+    font-family: Meiryo;
+  }
+  div {
+  background: #ffffff;
+  width: 500px;
+  padding: 10px;
+  text-align: center;
+  border: 1px solid #cccccc;
+  margin: 30px auto;
+  border-radius: 20px
+}
+</style>
+  </head>
+  <body>
+    <div>
+    <h1>Hello Heroku!!!</h1>
+    <p>はじめてのHeroku</p>
+  </div>
+  </body>
+</html>
+
+
